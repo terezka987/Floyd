8865f5226da7d79c910005ce5b6ddbb1b999fde7
Tereza Gonsiorova <tereza@Terezas-MacBook-Air.local>
2024-02-24 13:15:31 +1100

    add recursive floyd

---
+++ src/recursive_floyd.py
@@ -0,0 +1,32 @@
+import sys
+import itertools
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+NO_PATH = sys.maxsize
+
+graph = [[0, 7, NO_PATH, 8],
+[NO_PATH, 0, 5, NO_PATH],
+[NO_PATH, NO_PATH, 0, 2],
+[NO_PATH, NO_PATH, NO_PATH, 0]]
+
+MAX_LENGTH = len(graph[0])
+def recursive_floyd(distance):
+    """
+    Recursive implementation of Floyd's algorithm
+    """
+    def return_min(start_node, end_node, intermediate):
+        #print(start_node,end_node,intermediate)
+        if intermediate < 0:
+            return distance[start_node][end_node]
+        return min(
+            return_min(start_node, end_node, intermediate - 1),
+            return_min(start_node, intermediate, intermediate - 1) + return_min(intermediate, end_node, intermediate - 1)
+        )
+    for start_node in range(MAX_LENGTH):
+        for end_node in range(MAX_LENGTH):
+            for intermediate in range(MAX_LENGTH):
+                distance[start_node][end_node] = return_min(start_node, end_node, intermediate)
+    pp.pprint(distance)
+pp.pprint(graph)
+recursive_floyd(graph)
