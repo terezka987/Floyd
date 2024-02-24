45ba960b2b1f80b3fe48d9fedf8d8a05a8df5e11
Tereza Gonsiorova <tereza@Terezas-MacBook-Air.local>
2024-02-24 16:55:47 +1100

    Adding more tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -36,6 +36,64 @@ class TestStringMethods(unittest.TestCase):
         pp.pprint(recursive_result)
         self.assertEqual(imperative_result, recursive_result)
 
+    def test_recursive_function_negative_weights(self):
+        pp = pprint.PrettyPrinter(indent=4)
+
+        NO_PATH = sys.maxsize
+        graph = [[0, -4, NO_PATH],
+            [0, 0, -4],
+            [-2, NO_PATH, 4]]
+
+        imperative_result = imperative_floyd.floyd(graph)
+        recursive_result = recursive_floyd.recursive_floyd(graph)
+        pp.pprint(imperative_result)
+        pp.pprint(recursive_result)
+        self.assertEqual(imperative_result, recursive_result)
+
+
+    def test_recursive_function_big(self):
+        pp = pprint.PrettyPrinter(indent=4)
+
+        NO_PATH = sys.maxsize
+        graph = [[0, 3, NO_PATH,4,4,1],
+            [0, 0, 5,3,NO_PATH,4],
+            [2, NO_PATH, 4,NO_PATH,NO_PATH,NO_PATH],
+            [0, 0, 5,3,NO_PATH,4],
+            [0, 0, 5,3,NO_PATH,4],
+            [0, 0, 5,3,NO_PATH,2]
+            ]
+
+        imperative_result = imperative_floyd.floyd(graph)
+        recursive_result = recursive_floyd.recursive_floyd(graph)
+        pp.pprint(imperative_result)
+        pp.pprint(recursive_result)
+        self.assertEqual(imperative_result, recursive_result)
+
+    def test_recursive_function_very_big(self):
+        pp = pprint.PrettyPrinter(indent=4)
+
+        NO_PATH = sys.maxsize
+        graph = [
+            [0, 3, NO_PATH,4,4,1,0, 3, NO_PATH,4,4,1],
+            [0, 0, 5,3,NO_PATH,4,0, 0, 5,3,NO_PATH,4],
+            [2, NO_PATH, 4,NO_PATH,NO_PATH,NO_PATH,0, 0, 5,3,NO_PATH,4],
+            [0, 0, 5,3,NO_PATH,4,3, 0, 5,3,NO_PATH,4],
+            [0, 0, 5,3,NO_PATH,4,0, 4, 5,3,NO_PATH,4],
+            [0, 0, 5,3,NO_PATH,2,0, 0, 5,3,NO_PATH,2],
+            [0, 3, NO_PATH,4,4,1,0, 3, NO_PATH,4,4,1],
+            [0, 0, 5,3,NO_PATH,4,0, 0, 5,3,NO_PATH,4],
+            [2, NO_PATH, 4,NO_PATH,NO_PATH,NO_PATH,0, 0, 5,3,NO_PATH,4],
+            [0, 0, 5,3,NO_PATH,4,3, 0, 5,3,NO_PATH,4],
+            [0, 0, 5,3,NO_PATH,4,0, 4, 5,3,NO_PATH,4],
+            [0, 0, 5,3,NO_PATH,2,0, 0, 5,3,NO_PATH,2]
+            ]
+
+        imperative_result = imperative_floyd.floyd(graph)
+        recursive_result = recursive_floyd.recursive_floyd(graph)
+        pp.pprint(imperative_result)
+        pp.pprint(recursive_result)
+        self.assertEqual(imperative_result, recursive_result)
+
     # def test_recursive_function_ge(self):
     #     pp = pprint.PrettyPrinter(indent=4)
     #     MAX_LENGTH = 4
